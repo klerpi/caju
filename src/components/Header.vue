35af98a4e9229d5a5034651d4f96fb387c150ac5
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2020-11-24 18:29:17 -0300

    Add header html skeleton

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
 section.header
-  h1 Hello, {{ name }}.
+  h1 {{ name }}
+  p {{ desiredPosition }}
+  .contact-info
+    span.icon-with-text
+      i.material-icons email
+      span {{ contactInfo.email }}
+    span.icon-with-text
+      i.material-icons sms
+      span {{ contactInfo.phone }}
+    span.icon-with-text
+      i.material-icons link
+      span {{ contactInfo.website }}
 </template>
 
 <script>
