a25ca502ced2849975bdeb007ea9a453be2da524
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2020-11-24 18:48:20 -0300

    Add header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
 section.header
   h1 {{ name }}
-  p {{ desiredPosition }}
+  p.accent-color {{ desiredPosition }}
   .contact-info
     span.icon-with-text
-      i.material-icons email
+      i.material-icons.accent-color email
       span {{ contactInfo.email }}
     span.icon-with-text
-      i.material-icons sms
+      i.material-icons.accent-color phone
       span {{ contactInfo.phone }}
     span.icon-with-text
-      i.material-icons link
+      i.material-icons.accent-color link
       span {{ contactInfo.website }}
 </template>
 
@@ -20,5 +20,19 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+h1 {
+  font-size: 42px;
+  font-weight: 800;
+}
+
+i {
+  font-size: 20px;
+}
+
+p {
+  font-weight: bold;
+  font-size: 22px;
+  margin-bottom: 14px;
+}
 </style>
