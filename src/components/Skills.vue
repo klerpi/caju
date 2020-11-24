49b3374466ba7ac39c9c4a75f64b0533a7a91ade
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2020-11-24 19:59:18 -0300

    Add Skills component html skeleton

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,6 +2,35 @@
 .resume-section
   h2(v-if="lang === 'br'") Habilidades
   h2(v-else) Skills
+
+  .skill-itens
+    .skill-item
+      i.material-icons.accent-color settings
+      .skill-item-info
+        h3(v-if="lang === 'br'") Linguagens de Programação
+        h3(v-else) Programming Languages
+        p {{ skills.programmingLanguages }}
+
+    .skill-item
+      i.material-icons.accent-color code
+      .skill-item-info
+        h3(v-if="lang === 'br'") Desenvolvimento Web
+        h3(v-else) Web Development
+        p {{ skills.webDevelopment }}
+
+    .skill-item
+      i.material-icons.accent-color construction
+      .skill-item-info
+        h3(v-if="lang === 'br'") Ferramentas
+        h3(v-else) Tools
+        p {{ skills.tools }}
+
+    .skill-item
+      i.material-icons.accent-color language
+      .skill-item-info
+        h3(v-if="lang === 'br'") Línguas
+        h3(v-else) Languages
+        p {{ skills.languages }}
 </template>
 
 <script>
