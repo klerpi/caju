fa00dc7e445ccbc16e9c03272f52ef868e08a7d3
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2020-11-25 15:21:51 -0300

    Add html skeleton to Experience component

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,4 +1,23 @@
 <template lang="pug">
+.resume-section
+  h2(v-if="lang === 'br'") Experiência
+  h2(v-else) Experience
+
+  .experience-items
+    .experience-item(
+      v-for="(experience, index) in experiences",
+      key="exp-{{index}}"
+    )
+      h3 {{ experience.title }}
+      h4 {{ experience.company }}
+      span.icon-with-text
+        i.material-icons.accent-color calendar_today
+        span {{ experience.timeSpan }}
+      ul.experience-highlights
+        li(
+          v-for="(highlight, index) in experience.highlights",
+          key="high-{{index}}"
+        ) {{ highlight }}
 </template>
 
 <script>
@@ -7,5 +26,5 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 </style>
