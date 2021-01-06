2449e099c7d191ea37889cf7c6e152157617e504
Daniel Andrade <6894717+klerpi@users.noreply.github.com>
2021-01-06 15:50:37 -0300

    Add Projects section to resume

---
+++ src/components/Projects.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.resume-section
+  h2(v-if="lang === 'br'") Projetos
+  h2(v-else) Projects
+
+  .project-items
+    .item(v-for="(project, index) in projects", key="exp-{{index}}")
+      h3 {{ project.title }}
+      h4.accent-color {{ project.company }}
+      ul.project-highlights
+        li(
+          v-for="(highlight, index) in project.highlights",
+          key="high-{{index}}"
+        ) {{ highlight }}
+</template>
+
+<script>
+export default {
+  props: ["lang", "projects"],
+};
+</script>
+
+<style lang="scss" scoped>
+i {
+  font-size: 16px;
+}
+
+.project-highlights {
+  margin-top: 6px;
+}
+</style>
